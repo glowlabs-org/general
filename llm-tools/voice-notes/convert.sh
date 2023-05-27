8d74ba666cd9cbfb27b2055e43c2586c6f5b2c64
David Vorick <david@glowlabs.org>
2023-05-27 16:59:45 -0400

    update convert.sh again

--- llm-tools/voice-notes/convert.sh
+++ llm-tools/voice-notes/convert.sh
@@ -6,25 +6,38 @@ output_folder="transcripts"
 # Create output folder if it doesn't exist
 mkdir -p "$output_folder"
 
+# Initialize an array to hold the file paths
+file_paths=()
+
 # Iterate through files in the input folder
 for file in "$input_folder"/*; do
     if [ -f "$file" ]; then
-        # Extract metadata using exiftool
-        datetime=$(exiftool -s3 -d "%Y:%m:%d %H:%M:%S" -CreateDate "$file")
-        
-        # Replace colons and spaces in datetime
-        formatted_datetime=${datetime//:/-}
-        formatted_datetime=${formatted_datetime// /-}
+        file_paths+=("$file")
+    fi
+done
 
-        # Remove the path and extension from the filename
-        filename=$(basename -- "$file")
-        extension="${filename##*.}"
-        filename="${filename%.*}"
+# Run the command with the file paths
+whisper "${file_paths[@]}" --model large-v2 --output_format tsv --output_dir "$output_folder"
 
-        # Create new filename
-        new_filename="${formatted_datetime}-${filename}.${extension}"
-        
-        # Convert audio to transcript
-        whisper "$file" --model large-v2 --output_format tsv --output_file "$output_folder/$new_filename"
+# After whisper processing, rename all files in the output directory based on their Create Date
+for file in "${file_paths[@]}"; do
+    # Extract metadata using exiftool
+    datetime=$(exiftool -s3 -d "%Y:%m:%d %H:%M:%S" -CreateDate "$file")
+    
+    # Replace colons and spaces in datetime
+    formatted_datetime=${datetime//:/-}
+    formatted_datetime=${formatted_datetime// /-}
+
+    # Remove the path and extension from the filename
+    filename=$(basename -- "$file")
+    extension="${filename##*.}"
+    filename="${filename%.*}"
+
+    # Create new filename
+    new_filename="${formatted_datetime}-${filename}.tsv"
+    
+    # Rename the output file
+    if [ -f "$output_folder/$filename.tsv" ]; then
+        mv "$output_folder/$filename.tsv" "$output_folder/$new_filename"
     fi
 done
