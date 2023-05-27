db948fdd7d9a852f0f1649c8f4fd693343149cab
David Vorick <david@glowlabs.org>
2023-05-27 16:47:27 -0400

    attempt to improve convert.sh to include dates

--- llm-tools/voice-notes/convert.sh
+++ llm-tools/voice-notes/convert.sh
@@ -1,20 +1,30 @@
 #!/bin/bash
 
 input_folder="audio-files"
-output_folder="transcrips"
+output_folder="transcripts"
 
 # Create output folder if it doesn't exist
 mkdir -p "$output_folder"
 
-# Initialize an array to hold the file names
-file_names=()
-
 # Iterate through files in the input folder
 for file in "$input_folder"/*; do
     if [ -f "$file" ]; then
-        file_paths+=("$file")
+        # Extract metadata using exiftool
+        datetime=$(exiftool -DateTimeOriginal -s3 "$file")
+        
+        # Replace colons and spaces in datetime
+        formatted_datetime=${datetime//:/-}
+        formatted_datetime=${formatted_datetime// /-}
+
+        # Remove the path and extension from the filename
+        filename=$(basename -- "$file")
+        extension="${filename##*.}"
+        filename="${filename%.*}"
+
+        # Create new filename
+        new_filename="${formatted_datetime}-${filename}.${extension}"
+        
+        # Convert audio to transcript
+        whisper "$file" --model large-v2 --output_format tsv --output_file "$output_folder/$new_filename"
     fi
 done
-
-# Run the command with the file paths
-whisper "${file_paths[@]}" --model large-v2 --output_format tsv --output_dir "$output_folder"
