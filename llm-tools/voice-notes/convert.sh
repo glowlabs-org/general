d536761f113b1a1f8a24f3688241bbb88708feda
David Vorick <david@glowlabs.org>
2023-05-27 17:15:23 -0400

    restructure the data format

--- llm-tools/voice-notes/convert.sh
+++ llm-tools/voice-notes/convert.sh
@@ -1,43 +1,54 @@
 #!/bin/bash
 
-input_folder="audio-files"
-output_folder="transcripts"
-
-# Create output folder if it doesn't exist
-mkdir -p "$output_folder"
+data_folder="data"
 
 # Initialize an array to hold the file paths
-file_paths=()
+declare -A file_paths
 
-# Iterate through files in the input folder
-for file in "$input_folder"/*; do
-    if [ -f "$file" ]; then
-        file_paths+=("$file")
+# Iterate through subdirectories in the data folder
+for sub_dir in "$data_folder"/*; do
+    if [ -d "$sub_dir" ]; then
+        input_folder="$sub_dir/audio-files"
+        output_folder="$sub_dir/transcripts"
+        
+        # Create output folder if it doesn't exist
+        mkdir -p "$output_folder"
+
+        # Iterate through files in the input folder
+        for file in "$input_folder"/*; do
+            if [ -f "$file" ]; then
+                # Store the file path and its output folder
+                file_paths["$file"]="$output_folder"
+            fi
+        done
     fi
 done
 
-# Run the command with the file paths
-whisper "${file_paths[@]}" --model large-v2 --output_format tsv --output_dir "$output_folder"
+# Run the whisper command with the file paths
+whisper "${!file_paths[@]}" --model large-v2 --output_format tsv --output_dir "/tmp"
 
-# After whisper processing, rename all files in the output directory based on their Create Date
-for file in "${file_paths[@]}"; do
+# After whisper processing, rename all files in the corresponding output directory based on their Create Date
+for file in "${!file_paths[@]}"; do
     # Extract metadata using exiftool
     datetime=$(exiftool -s3 -d "%Y:%m:%d %H:%M:%S" -CreateDate "$file")
     
     # Replace colons and spaces in datetime
     formatted_datetime=${datetime//:/-}
     formatted_datetime=${formatted_datetime// /-}
-
+    
     # Remove the path and extension from the filename
     filename=$(basename -- "$file")
     extension="${filename##*.}"
     filename="${filename%.*}"
-
+    
     # Create new filename
     new_filename="${formatted_datetime}-${filename}.tsv"
     
+    # Define the output directory for this file
+    output_folder=${file_paths["$file"]}
+    
     # Rename the output file
-    if [ -f "$output_folder/$filename.tsv" ]; then
-        mv "$output_folder/$filename.tsv" "$output_folder/$new_filename"
+    if [ -f "/tmp/$filename.tsv" ]; then
+        mv "/tmp/$filename.tsv" "$output_folder/$new_filename"
     fi
 done
