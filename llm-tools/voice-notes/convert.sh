44f149fa9cd923a328296494e52d5c2f7d9778ba
David Vorick <david@glowlabs.org>
2023-05-27 13:15:11 -0400

    update convert script to only need to load model once

--- llm-tools/voice-notes/convert.sh
+++ llm-tools/voice-notes/convert.sh
@@ -6,19 +6,16 @@ output_folder="transcriptions"
 # Create output folder if it doesn't exist
 mkdir -p "$output_folder"
 
+# Initialize an array to hold the file names
+file_names=()
+
 # Iterate through files in the input folder
 for file in "$input_folder"/*; do
     if [ -f "$file" ]; then
         filename=$(basename "$file")
-        extension="${filename##*.}"
-        filename="${filename%.*}"
-
-        output_file="$output_folder/$filename.tsv"
-
-        # Run the command with the current file
-        whisper "$file" --model large-v2 --output_format tsv --output_dir "$output_folder"
-
-        echo "Transcription saved to: $output_file"
+        file_names+=("$filename")
     fi
 done
 
+# Run the command with the file names
+whisper "${file_names[@]}" --model large-v2 --output_format tsv --output_dir "$output_folder"
