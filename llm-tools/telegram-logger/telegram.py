5da7db82d554a95d941ecfc2c04e071fad9561ad
DavidVorick <david.vorick@gmail.com>
2023-10-15 03:29:35 -0400

    only get 500 dialogs

--- llm-tools/telegram-logger/telegram.py
+++ llm-tools/telegram-logger/telegram.py
@@ -35,7 +35,7 @@ async def main():
             print("Signed in successfully.")
 
         # Fetch a list of chat histories, called 'dialogs'.
-        all_dialogs = await client.get_dialogs(limit=2850)
+        all_dialogs = await client.get_dialogs(limit=500)
 
         # Create the 'conversations' folder, which is where we will store each
         # of our conversations, one file per conversation.
